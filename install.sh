bc2eec170afe1d61f2a7871bca4b2748b6d3adf9
Aaron Fabbri <ajfabbri@gmail.com>
2013-11-11 16:36:10 -0800

    Improve how to setup .vim/.vimrc from git repo.

--- install.sh
+++ install.sh
@@ -7,12 +7,17 @@
 # Step 2: Run this script.
 #
 
-# TODO
-GIT_URL=""
-exit
+GIT_URL="https://github.com/ajfabbri/vim-config.git"
 VIM_INSTALL_DIR=Vim
 
-if [ ! -d "$GIT_REPO_BASE" ]
+die() {
+    printf >&2 -- '%s' "$1"
+    exit ${2:-1}
+}
+
+cd $HOME || die chdir
+
+if [ ! -d "$VIM_INSTALL_DIR" ]
 then
 	echo "Didn't find new Vim config.  Try running this in your home dir:"
 	echo "  git clone $GIT_URL $VIM_INSTALL_DIR"
@@ -22,10 +27,12 @@ fi
 
 echo "Moving existing .vim/ and .vimrc into .vimbackup/."
 echo "Feel free to manually copy old stuff into the new .vim*"
-chdir $HOME
-mkdir .vimbackup
+mkdir .vimbackup || die mkdir
+mv .vim .vimbackup || die 
+mv .vimrc .vimbackup || die
 
 echo "Symlinking to new goodness in $VIM_INSTALL_DIR."
-mv .vim .vimbackup
-mv .vimrc .vimbackup
+ln -s Vim .vim || die ln
+ln -s Vim/.vimrc .vimrc || die ln
+
 
