783692f893fc60b9c143d517fd398e89f80608ac
Aaron Fabbri <ajfabbri@gmail.com>
2023-06-20 17:29:18 -0700

    nvim: customize LazyVim

---
+++ .config/nvim/lua/plugins/code.lua
@@ -0,0 +1,50 @@
+return {
+
+  -- extend auto completion
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      table.insert(cmp.mapping.preset, {
+        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      })
+      table.insert(cmp.mapping.preset, {
+        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      })
+    end,
+  },
+
+  -- tidy: auto-remove whitespace errors
+  {
+    "mcauley-penney/tidy.nvim",
+    event = "VeryLazy",
+    opts = {
+      filetype_exclude = { "markdown", "diff" },
+    },
+  },
+
+  -- treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {
+          "comment",
+          "diff",
+          "dockerfile",
+          "dot",
+          "git_rebase",
+          "gitattributes",
+          "gitcommit",
+          "gitignore",
+          "graphql",
+          "http",
+          "jq",
+          "make",
+          "sql",
+        })
+      end
+    end,
+  },
+}
---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    enabled = false,
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+}
