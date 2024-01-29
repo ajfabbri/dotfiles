e6bb1b46dbb13544c5654399e29a7cab3419ce65
Aaron Fabbri <ajfabbri@gmail.com>
2024-01-29 11:47:40 -0800

    neovim: fix issues in filetype filter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -164,7 +164,7 @@ require('copilot').setup({
         go = true,
         yaml = true,
         sh = function()
-            if string.match(vim.fn.basname(vim.fn.nvim_buf_get_name(0)), '^%.env.*') then
+            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
                 -- disable for .env files
                 return false
             end
